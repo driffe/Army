7e34df8ee62f18a18405043f2556a9d2bfda787d
SeyoungOh <33598679+driffe@users.noreply.github.com>
2023-12-06 14:05:18 +0000

    create auth file

--- money-tracker/app/layout.js
+++ money-tracker/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Nav from '@/components/Navigation'
 import FinanceContextProvider from '@/lib/store/finance-context'
+import AuthContextProvider from '@/lib/store/auth-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +15,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <FinanceContextProvider>
-          <Nav/> 
-          {children}
-        </FinanceContextProvider>
+        <AuthContextProvider>
+          <FinanceContextProvider>
+            <Nav/> 
+            {children}
+          </FinanceContextProvider>
+        </AuthContextProvider>
       </body>
     </html>
   )
--- money-tracker/components/Navigation.js
+++ money-tracker/components/Navigation.js
@@ -1,31 +1,42 @@
+import { useContext } from 'react'
+import { authContext } from '@/lib/store/auth-context'
 import {ImStatsBars} from 'react-icons/im'
 
 function Nav(){
+    const {user, loading, logout} = useContext(authContext);
     return (
         <header className='container max-w-2xl px-6 py-6 mx-auto'>
           <div className='flex items-center justify-between'>
             {/* User Info */}
-            <div className='flex items-center gap-4'>
-              {/* img */}
-              <div className='h-[40px] w-[40px] rounded-full overflow-hidden'>
-                <img
-                  className='object-cover w-full h-full'
-                  src="/profile.jpg"
-                  alt="Profile Img"
-                />
+            {user && !loading && (
+              <div className='flex items-center gap-4'>
+                {/* img */}
+                <div className='h-[40px] w-[40px] rounded-full overflow-hidden'>
+                  <img
+                    className='object-cover w-full h-full'
+                    src={user.photoURL}
+                    alt={user.displayName}
+                    referrerPolicy='no-referrer'
+                  />
+                </div>
+      
+                {/* name */}
+                <small>Hi, {user.displayName}!</small>
               </div>
-    
-              {/* name */}
-              <small>Hi, Leon!</small>
-            </div>
+            )}
     
             {/* Right side fo nav */}
-            <nav className='flex items-center gap-2'>
-              <div><ImStatsBars className='text-2xl text-white' /></div>
-              <div><button className='btn btn-danger'>Sign out</button></div>
-            </nav>        
+            {user && !loading && (
+              <nav className='flex items-center gap-2'>
+                <div>
+                  <ImStatsBars className='text-2xl text-white'/>
+                  </div>
+                <div>
+                  <button onClick={logout} className='btn btn-danger'>Sign out</button>
+                </div>
+              </nav>        
+            )}
           </div>
-    
         </header>    
       ) 
 }
